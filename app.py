2838eee97557ba4878124dea72014221d22c279e
Ubuntu <ubuntu@ip-172-31-91-93.ec2.internal>
2019-12-12 16:29:26 +0000

    create app route for adding plants

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -11,8 +11,10 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://sianjade:druidfa
 mongo = PyMongo(app)
 
 @app.route('/')
+
+@app.route('/add_plant')
 def hello():
-    return 'Hello world'
+    return render_template("add_plant.html", plant=mongo.db.plants.find())
     
     
 if __name__ == '__main__':
