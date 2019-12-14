664b818bd4505b82b0a9c62fedaeea00bad66c19
Ubuntu <ubuntu@ip-172-31-91-93.ec2.internal>
2019-12-14 20:46:26 +0000

    Add app routes for inserting plants and viewing plants

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'PlantPal'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://sianjade:druidfan95@myfirstcluster-0vxhc.mongodb.net/PlantPal?retryWrites=true&w=majority')
+app.config["MONGO_URI"] = 'mongodb+srv://sianjade:druidfan95@myfirstcluster-0vxhc.mongodb.net/PlantPal?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
 @app.route('/')
 
 @app.route('/add_plant')
-def hello():
-    return render_template("add_plant.html", plant=mongo.db.plants.find())
+def add_plant():
+    return render_template("add_plant.html", add_plant=mongo.db.plants.find())
     
     
 if __name__ == '__main__':
